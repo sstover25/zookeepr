2416455dac77ff79d1e99d5473dc340bd63f5dcb
SSto <95592688+sstover25@users.noreply.github.com>
2022-03-02 12:56:05 -0600

    added front-end functionality and animal display

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.static("public"));
+
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
@@ -110,6 +112,22 @@ app.post("/api/animals", (req, res) => {
 
     res.json(animal);
   }
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/animals", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/animals.html"));
+});
+
+app.get("/zookeepers", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/zookeepers.html"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, ".public/index.html"));
 });
 
 app.listen(PORT, () => {
